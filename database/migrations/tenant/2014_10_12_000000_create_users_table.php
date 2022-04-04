5e819f89f14953bcb43b573c5852c42c508b546a
sameh <smta55@hotmail.com>
2022-04-04 23:06:19 +0200

    impelment repository patern

--- database/migrations/tenant/2014_10_12_000000_create_users_table.php
+++ database/migrations/tenant/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,21 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            
+            $table->text('avatar')->nullable();
+            $table->string('phone')->nullable();
+            $table->string('facebook_id')->nullable();
+            $table->string('google_id')->nullable();
+            $table->string('firebase_token')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
+            $table->tinyInteger('is_active')->nullable()->default(1);
+            $table->tinyInteger('is_admin')->nullable()->default(0);
+            $table->enum('gender', ['mail','femail'])->nullable();
+            $table->integer('user_type_id')->nullable();
+            $table->date('dop')->nullable();
+            $table->tinyInteger('sms_notification')->nullable();
+            $table->string('lang')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
